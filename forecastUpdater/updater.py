7c27b9a7d80301d56fea3b38a8c6146d2c3ff9c2
BhavyaBibi <bhavyabibi007@gmail.com>
2022-06-13 14:43:01 +0400

    the api update section also added

---
+++ forecastUpdater/updater.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import os
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from forecastUpdater import forecast_api
+
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(forecast_api.update_forecast, 'interval', minutes=45)
+    scheduler.start()
